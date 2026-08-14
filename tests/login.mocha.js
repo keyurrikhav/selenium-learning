e81a87a04bae1c61c6cadc922fb15aea3bdd59a6
keyurrikhav <keyur.rikhav@gmail.com>
2026-08-14 10:22:53 +0530

    mocha/selenium

---
+++ tests/login.mocha.js
@@ -0,0 +1,14 @@
+const assert = require("assert");
+
+describe("Login Test Suite", function () {
+
+    it("should perform a login test", function () {
+
+        assert.strictEqual(
+            1,
+            1
+        );
+
+    });
+
+});
