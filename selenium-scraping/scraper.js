078f5bc7a1b3bbff702956ec4696c1d7da431a87
keyurrikhav <keyur.rikhav@gmail.com>
2026-08-27 12:34:27 +0530

    get all product details useing web scraping

--- selenium-scraping/scraper.js
+++ selenium-scraping/scraper.js
@@ -10,8 +10,27 @@ async function scrapeProducts() {
 
         await driver.get("https://books.toscrape.com/");
 
-        console.log("Website opened");
+        // console.log("Website opened");
+        const books = await driver.findElements(
+            By.css(".product_pod")
+        );
 
+         console.log("Total books:", books.length);
+
+          for (const book of books)
+       {
+         const title = await book
+            .findElement(By.css("h3 a"))
+            .getText();
+
+        const price = await book
+            .findElement(By.css(".price_color"))
+            .getText();
+
+            console.log({
+                title,price
+            });
+       }
     } finally {
 
         await driver.quit();
